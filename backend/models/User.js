39e1ca0ee7b17cee3426e1da74f146e122594b4a
DarthWader96 <shaokahn848@gmail.com>
2026-04-24 14:40:11 +0500

    9.models/User.js o'zgartirildi

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: { type: String, required: true },
     customId: { type: String, unique: true, required: true }, // Zangi ilovasidagidek noyob ID
-    phone: { type: String, unique: true, sparse:true }, // Kontaktlar orqali topish uchun
+    phone: { type: String, required:true, unique: true, sparse:true }, // Kontaktlar orqali topish uchun
     status: { type: String, default: "offline" },
     lastSeen: { type: Date, default: Date.now }, // Oxirgi faollik vaqti
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    avatar: { type: String, default: "" },
 })
 
 module.exports = mongoose.model("User", userSchema)
